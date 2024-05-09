7259f4c65ebeaa2172a5ff4c8bafef7f913d588d
Kevin Angulo Lezama <kevinjangulol@gmail.com>
2024-05-09 10:43:51 -0400

    changes

--- react-portfolio/src/components/Home/Logo/index.js
+++ react-portfolio/src/components/Home/Logo/index.js
@@ -1,12 +1,20 @@
 import './index.scss';
-import LogoS from '../../../assets/images/';
+import LogoS from '../../../assets/images/front.png';
+import {useRef} from 'react'
 
 
 const Logo = () => {
+    const bgRef = useRef()
+    const solidLogoRef = useRef()
+    const outlineLogoRef = useRef()
     return(
         <div className="logo-container">
             <img className="solid-logo" src={LogoS} alt='Kevin'/>
+
+
+            
         </div>
+        
     )
 
 }
