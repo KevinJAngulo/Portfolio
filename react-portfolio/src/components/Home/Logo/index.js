e64d0b79c848b8b245e58ff300e8d49f92871912
Kevin Angulo Lezama <kevinjangulol@gmail.com>
2024-05-09 10:15:04 -0400

    s

---
+++ react-portfolio/src/components/Home/Logo/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+import LogoS from '../../../assets/images/';
+
+
+const Logo = () => {
+    return(
+        <div className="logo-container">
+            <img className="solid-logo" src={LogoS} alt='Kevin'/>
+        </div>
+    )
+
+}
+
+export default Logo
